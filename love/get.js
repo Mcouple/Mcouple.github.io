3f137a4cb512d8ededa8060e03779b795d00942c
Mcouple <suplzy0409@gmail.com>
2020-04-14 12:28:51 +0800

    Site updated: 2020-04-14 12:28:48

---
+++ love/get.js
@@ -0,0 +1,1 @@
+var xhr=new XMLHttpRequest();xhr.open("get","https://v1.hitokoto.cn");xhr.onreadystatechange=function(){if(xhr.readyState===4){var data=JSON.parse(xhr.responseText);var hitokoto=document.getElementById("hitokoto");hitokoto.innerText=data.hitokoto}};xhr.send();
